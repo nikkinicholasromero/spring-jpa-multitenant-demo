11cd5134a332ef48849f2f52f2c06ed7ad13aad6
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2022-09-02 07:28:45 +0800

    Minor cleanup

--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -10,19 +10,16 @@ import java.util.stream.Collectors;
 
 @Configuration
 public class AppConfig {
-    @Bean("dataSource")
-    public MultitenantDataSource dataSource(TenantDataSourceProperties properties, Map<Object, Object> dataSources) {
+    @Bean
+    public MultitenantDataSource dataSource(TenantDataSourceProperties properties) {
+        Map<String, DataSourceProperties> dataSourceProperties = properties.getDatasources();
+        Map<Object, Object> dataSources = dataSourceProperties.entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> createDataSource(e.getValue())));
+
         MultitenantDataSource dataSource = new MultitenantDataSource();
         dataSource.setDefaultTargetDataSource(createDataSource(properties.getDatasource()));
         dataSource.setTargetDataSources(dataSources);
         return dataSource;
-    }
-
-    @Bean
-    public Map<Object, Object> dataSources(TenantDataSourceProperties properties) {
-        Map<String, DataSourceProperties> dataSourceProperties = properties.getDatasources();
-        return dataSourceProperties.entrySet().stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, e -> createDataSource(e.getValue())));
     }
 
     private DataSource createDataSource(DataSourceProperties dataSourceProperties) {
